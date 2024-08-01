ec115c92cfcc273405f754da2107c5350cfd8ca3
DominicSilva <dominicsilva606@gmail.com>
2024-08-01 01:20:15 -0500

    Subiendo primeros cambios

--- Cajero/CuentaBancaria.cs
+++ Cajero/CuentaBancaria.cs
@@ -1,8 +1,23 @@
-﻿internal class CuentaBancaria
-{
-    public int idCuenta;
-    public double saldo;
-    public List<double> movimientos;
-    private string clave;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
+namespace CajeroAutomatico
+{
+    public class CuentaBancaria
+    {
+        public string Usuario { get; private set; }
+        public string Clave { get; private set; }
+        public double Saldo { get; private set; }
+        private static string archivoUsuarios = "C:\\Users\\Windows\\source\\repos\\Cajero\\Cajero\\bin\\Debug\\Listas.txt";
+        private string archivoMovimientosUsuario;
+
+        public CuentaBancaria(string usuario, string clave, double saldo)
+        {
+            Usuario = usuario;
+            Clave = clave;
+            Saldo = saldo;
+            archivoMovimientosUsuario = $"{usuario}_Movimientos.txt";
+        }
+    }
 }
--- Cajero/Program.cs
+++ Cajero/Program.cs
@@ -1,54 +1,45 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Collections.Generic;
 
 namespace CajeroAutomatico
 {
     class Program
     {
-        public static string usuarioActual;
-        private static string archivoUsuarios = "C:\\Users\\Windows\\source\\repos\\Cajero\\Cajero\\bin\\Debug\\Listas.txt";
-        private static string archivoMovimientosUsuario ;
+        static CuentaBancaria cuenta;
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Inicio();
-
         }
 
         public static void Inicio()
         {
-            Console.WriteLine("Ingrese ID de cuenta ");
+            Console.WriteLine("Ingrese ID de cuenta: ");
             string usuario = Console.ReadLine();
-            Console.WriteLine("ingrese Contraseña");
+            Console.WriteLine("Ingrese Contraseña: ");
             string clave = Console.ReadLine();
 
-
-            if (ValidarCredenciales(usuario, clave))
+            if (cuenta == null)
             {
-                usuarioActual = usuario;
+                string[] usuarios = System.IO.File.ReadAllLines("C:\\Users\\Windows\\source\\repos\\Cajero\\Cajero\\bin\\Debug\\Listas.txt");
+                foreach (string linea in usuarios)
+                {
+                    string[] datos = linea.Split(',');
+                    if (datos[0] == usuario && datos[1] == clave)
+                    {
+                        cuenta = new CuentaBancaria(datos[0], datos[1], Convert.ToDouble(datos[2]));
+                        break;
+                    }
+                }
+            }
+
+            if (cuenta != null && cuenta.ValidarCredenciales(usuario, clave))
+            {
                 Menu();
             }
             else
             {
                 Console.WriteLine("ID de cuenta o Contraseña incorrecta, Intentelo de nuevo");
-
             }
-        }
-
-        private static bool ValidarCredenciales(string usuario, string clave)
-        {
-            string[] usuarios = File.ReadAllLines(archivoUsuarios);
-            foreach (string linea in usuarios)
-            {
-                string[] datos = linea.Split(',');
-                if (datos[0] == usuario && datos[1] == clave  )
-                {
-                    return true;
-                }
-            }
-            return false;
         }
 
         private static void Menu()
@@ -67,158 +58,34 @@ namespace CajeroAutomatico
                 switch (operacion)
                 {
                     case 1:
-                        RetirarDinero();
+                        Console.WriteLine("Ingrese la cantidad a retirar:");
+                        double cantidadRetiro = Convert.ToDouble(Console.ReadLine());
+                        cuenta.Retirar(cantidadRetiro);
                         break;
                     case 2:
-                        DepositarDinero();
+                        Console.WriteLine("Ingrese la cantidad a depositar:");
+                        double cantidadDeposito = Convert.ToDouble(Console.ReadLine());
+                        cuenta.Depositar(cantidadDeposito);
                         break;
                     case 3:
-                        CambioClave();
+                        Console.WriteLine("Ingrese su nueva contrasena:");
+                        string nuevaClave = Console.ReadLine();
+                        cuenta.CambiarClave(nuevaClave);
                         break;
                     case 4:
-                        ConsultarSaldo();
+                        cuenta.ConsultarSaldo();
                         break;
                     case 5:
-                        VerMovimientos();
+                        cuenta.VerMovimientos();
                         break;
                     case 6:
                         Console.WriteLine("Saliendo del programa...");
                         return;
-                     default:
+                    default:
                         Console.WriteLine("Opcion no valida. Intenta de nuevo");
                         break;
-                        
                 }
-
-
-
-            }
-
-        }
-        private static void ConsultarSaldo()
-        {
-            string[] usuarios = File.ReadAllLines(archivoUsuarios);
-            foreach (string linea in usuarios)
-            {
-                string[] datos = linea.Split(',');
-                if (datos[0] == usuarioActual)
-                {
-                    Console.WriteLine($"su saldo es: { datos[2]}");
-                }
-            }
-        }
-
-        private static void RetirarDinero()
-        {
-            Console.WriteLine("Ingrese la cantidad a retirar");
-            double cantidad = Convert.ToDouble(Console.ReadLine());
-
-            RegistrarMovimiento(usuarioActual, $"Retiro de ${cantidad}");
-
-            List<string> lineas = new List<string>();
-            string[] usuarios = File.ReadAllLines(archivoUsuarios);
-            foreach(string linea in usuarios)
-            {
-                string[] datos = linea.Split(',');
-                if (datos[0] == usuarioActual)
-                {
-                    double saldo = Convert.ToDouble(datos[2]);
-                    if (cantidad <= saldo)
-                    {
-                        saldo -= cantidad;
-                        datos[2] = saldo.ToString();
-                        Console.WriteLine($"Ha retidardo {cantidad}. Su nuevo saldo es {saldo}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Saldo insuficiente");
-                    }
-                }
-                lineas.Add(string.Join(",", datos));
-            }
-            File.WriteAllLines(archivoUsuarios, lineas);
-        }
-
-
-        private static void DepositarDinero()
-        {
-            Console.WriteLine("Ingrese la cantidad a depositar:");
-            double cantidad = Convert.ToDouble(Console.ReadLine());
-
-            RegistrarMovimiento(usuarioActual, $"Deposito de ${cantidad}");
-
-            List<string> lineas = new List<string>();
-            string[] usuarios = File.ReadAllLines(archivoUsuarios);
-            foreach (string linea in usuarios)
-            {
-                string[] datos = linea.Split(',');
-                if (datos[0] == usuarioActual)
-                {
-                    double saldo = Convert.ToDouble(datos[2]);
-                    saldo += cantidad;
-                    datos[2] = saldo.ToString();
-                    Console.WriteLine($"Ha depositado {cantidad}. Su nuevo saldo es {saldo}");
-                }
-                lineas.Add(string.Join(",", datos));
-            }
-            File.WriteAllLines(archivoUsuarios, lineas);
-        }
-
-        private static void CambioClave()
-        {
-            Console.WriteLine("Ingrese su nueva contrasena");
-            string nuevaClave = Console.ReadLine();
-
-            List<string> lineas = new List<string>();
-            string[] usuarios = File.ReadAllLines(archivoUsuarios);
-            foreach (string linea in usuarios)
-            {
-                string[] datos = linea.Split(",");
-                if (datos[0] == usuarioActual)
-                {
-                    datos[1] = nuevaClave;
-                    Console.WriteLine("Contrasena cambiada exitosamente.");
-                }
-                lineas.Add(string.Join(",", datos));
-            }
-            File.WriteAllLines(archivoUsuarios, lineas);
-        }
-
-        private static void RegistrarMovimiento(string usuario, string movimiento)
-        {
-            archivoMovimientosUsuario = $"{usuario}_Movimientos.txt";
-
-            if (!File.Exists(archivoMovimientosUsuario))
-            {
-                using (StreamWriter escriba = File.CreateText(archivoMovimientosUsuario))
-                {
-                    escriba.WriteLine($"{DateTime.Now} - {movimiento} - {usuarioActual}");
-                }
-            }
-
-            using (StreamWriter escriba = File.AppendText(archivoMovimientosUsuario))
-            {
-                escriba.WriteLine($"{DateTime.Now} - {movimiento} - {usuarioActual}");
-            }
-        }
-
-        private static void VerMovimientos()
-        {
-            archivoMovimientosUsuario = $"{usuarioActual}_Movimientos.txt";
-
-            if (File.Exists(archivoMovimientosUsuario))
-            {
-                string[] movimientos = File.ReadAllLines (archivoMovimientosUsuario);
-                foreach (string movimiento in movimientos)
-                {
-                    Console.WriteLine (movimiento);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No hay movimientos para mostrar");
             }
         }
     }
-    
 }
